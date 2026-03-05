d04a5c046044e026502dc06d4a51c9ffefbe289a
Pranav <latanaikude2@gmail.com>
2026-03-05 22:04:32 +0530

    fix: allow localhost:5174 in CORS whitelist

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(cors({
     const allowedOrigins = [
       process.env.FRONTEND_URL,
       'http://localhost:5173',
+      'http://localhost:5174',
       'http://localhost:3000',
       'capacitor://localhost',
       'http://localhost',  // Android WebView origin
